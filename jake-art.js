dc3b7420ea4034f7e5c9db0eff12194933e94ca5
Benjamin <spotswoodb@gmail.com>
2022-12-10 12:17:49 -0500

    audio is playing over image

--- jake-art.js
+++ jake-art.js
@@ -1,16 +1,26 @@
 let img;
 let cnv;
+let song, buttton, fft, space_between_lines;
+
+function toggleSong() {
+  if(song.isPlaying()) {
+    song.pause();
+  } else {
+    song.play();
+  }
+}
 
 function preload(){
-img = loadImage('assets/jake_ncaas.jpg');
+  img = loadImage('assets/jake_ncaas.jpg');
+  song = loadSound('assets/Ima Boss.mp3'); 
 }
 
 //only run once
 function setup() {
   cnv = createCanvas (img.width, img.height);
   //print(ing.width,img.height)
-  let newCanvasX = (windowWidth - img.width)/2;
-  let newCanvasY = (windowHeight - img.height)/2;
+  let newCanvasX = (windowWidth - img.width)/10;
+  let newCanvasY = (windowHeight - img.height)/10;
   cnv.position(newCanvasX, newCanvasY);
   //access the pixel information of the image
   for(let col = 0; col< img.width; col+=2) {
@@ -26,9 +36,35 @@ function setup() {
       strokeWeight(random(5));
       point(xPos, yPos);
       strokeWeight(random(3));
-      curve(xPos, yPos, sin(xPos) * random(60), cos(xPos) * sin(xPos) * random(90),
-           random(10), random(80), cos(yPos) * sin(yPos) * random(140), cos(xPos) * sin(xPos) * 50)
+      curve(xPos, yPos, sin(xPos) * random(30), cos(xPos) * sin(xPos) * random(70),
+           random(10), random(30), cos(yPos) * sin(yPos) * random(30), cos(xPos) * sin(xPos) * 100)
       pop();
     }
   }
+
+  angleMode(DEGREES); // Change the mode to DEGREES
+  colorMode(HSB);
+  buttton = createButton('Toggle Play');
+  buttton.mousePressed(toggleSong);
+  song.play();  
+  fft = new p5.FFT(0.9, 128);
+  space_between_lines = width / 128;
+}
+
+function draw() {
+  // background(0);
+  
+  let spectrum = fft.analyze();
+  for (let i = 0; i < spectrum.length; i++) {
+    fill(i,255,255);
+    let amp = spectrum[i];
+    let y = map(amp, 0, 256, height, 0);
+    rect(i * space_between_lines, y, space_between_lines, height - y);
+  }
+}
+
+// Chrome 70 will require user gestures to enable web audio api
+// Click on the web page to start audio
+function touchStarted() {
+  getAudioContext().resume();
 }
