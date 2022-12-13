2ef6a3ab2f02a370a74e00a5535b507c6724bba7
Benjamin <spotswoodb@gmail.com>
2022-12-13 12:42:30 -0500

    get access to pixels of an image

--- jake-art-with-music.js
+++ jake-art-with-music.js
@@ -1,77 +1,45 @@
-let img;
-let cnv;
-let song;
-let fft;
-
-// function toggleSong() {
-//   if(song.isPlaying()) {
-//     song.pause();
-//   } else {
-//     song.play();
-//   }
-// }
+var fft;
+var audio;
+var img;
 
 function preload(){
-    img = loadImage('assets/jake_ncaas.jpg');
-    song = loadSound('assets/Ima Boss.mp3'); 
+    img = loadImage('assets/jake-fifth-prototype-with-first-code-final.png');
+    fft = new p5.FFT()
+    audio = loadSound('assets/Ima Boss.mp3');
 }
 
 //only run once
 function setup() {
-    
-    cnv = createCanvas(img.width, img.height);
-    pixelDensity(1);
-    let newCanvasX = (windowWidth - img.width)/2;
-    let newCanvasY = (windowHeight - img.height)/2;
-    cnv.position(newCanvasX, newCanvasY);
-    // access the pixel information of the image
-    for(let col = 0; col< img.width; col+=2) {
-        for(let row = 0; row<img.height; row+=2){
-        let xPos = col;
-        let yPos = row;
-        let c = img.get(xPos,yPos);
-        push();
-        translate(xPos, yPos);
-        rotate(radians(random(360)))
-        noFill();
-        stroke(color(c))
-        strokeWeight(random(5));
-        point(xPos, yPos);
-        strokeWeight(random(3));
-        curve(xPos, yPos, sin(xPos) * random(30), cos(xPos) * sin(xPos) * random(70),
-            random(10), random(30), cos(yPos) * sin(yPos) * random(30), cos(xPos) * sin(xPos) * 100)
-        pop();
-        }
-    }
-
+    audio.play();
+    createCanvas(800, 600);
+    let p = displayDensity();
+    pixelDensity(p);
     img.loadPixels();
-
-    song.play();
-    fft = new p5.FFT();
-    fft.setInput(song);
-
+    image(img, 0, 0, width, height);
+    // updatePixels();
 }
 
 function draw() {
-    noFill();
-    fft.analyze();
-    var spectrum = fft.getEnergy("bass");
+    var spectrum = fft.analyze();
 
-    for(var y = 0; y < img.height; y++) {
-        for(var x = 0; x < img.width; x++) {
-            var pixelIndex = (y * img.width + x) * 4;
-            img.pixels[pixelIndex] += spectrum;
-            img.pixels[pixelIndex + 1] += spectrum;
-            img.pixels[pixelIndex + 2] += spectrum;
+    for (var x = 0; x < width; x++) {
+        for (var y = 0; y < height; y++) {
+          // Get the frequency at the current pixel's x-coordinate
+          var index = (x + y * width) * 4;
+
+          var freq = spectrum[x];
+
+          pixels[index] = freq;
+          pixels[index + 1] = 0;
+          pixels[index + 2] = 0;
+          pixels[index + 3] = 255;
+
         }
+      }
+
+      img.updatePixels();
     }
 
-    img.updatePixels();
-
-    image(img, 0, 0);  
-
-
-}
 
 // // Chrome 70 will require user gestures to enable web audio api
 // // Click on the web page to start audio
@@ -85,3 +53,19 @@ function touchStarted() {
 //     img.pixels[i + 1] = spectrum[i + 1];
 //     img.pixels[i + 2] = spectrum[i + 2];
 // }
+
+
+// background(img);
+// fft.analyze();
+// var spectrum = fft.getEnergy("bass");
+
+// for(var y = 0; y < img.height; y++) {
+//     for(var x = 0; x < img.width; x++) {
+//         var pixelIndex = (y * img.width + x) * 4;
+//         img.pixels[pixelIndex] += spectrum;
+//         img.pixels[pixelIndex + 1] += spectrum;
+//         img.pixels[pixelIndex + 2] += spectrum;
+//     }
+// }
+
+// image(img, 0, 0);  
